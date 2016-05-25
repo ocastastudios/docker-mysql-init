1cfbaa6438ba6bc0830950f8807eef7cbb384c65
Jason Fry <jason@jasonfry.co.uk>
2016-05-25 13:41:42 +0100

    connection timeout

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-mysql -u $ROOT_USER -p$ROOT_PASSWORD -h $SERVER -e "CREATE DATABASE $NEW_DATABASE;"
-mysql -u $ROOT_USER -p$ROOT_PASSWORD -h $SERVER -e "grant usage on *.* to $NEW_USER identified by '$NEW_PASSWORD';"
-mysql -u $ROOT_USER -p$ROOT_PASSWORD -h $SERVER -e "grant all privileges on $NEW_DATABASE.* to $NEW_USER;"
+mysql -u $ROOT_USER -p$ROOT_PASSWORD -h $SERVER --connect_timeout 10 -e "CREATE DATABASE $NEW_DATABASE;"
+mysql -u $ROOT_USER -p$ROOT_PASSWORD -h $SERVER --connect_timeout 10 -e "grant usage on *.* to $NEW_USER identified by '$NEW_PASSWORD';"
+mysql -u $ROOT_USER -p$ROOT_PASSWORD -h $SERVER --connect_timeout 10 -e "grant all privileges on $NEW_DATABASE.* to $NEW_USER;"
